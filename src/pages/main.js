28812fe540150838937b67498255d3abb8cda3cd
FelixTisov <fedya002.ks@gmail.com>
2022-04-19 23:04:28 +0300

    добавлены основные страницы

---
+++ src/pages/main.js
@@ -0,0 +1,35 @@
+import './main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <body>
+        {/* Главная часть */}
+        <div>
+          {/* header */}
+          <div>
+
+          </div>
+          {/* Основная часть */}
+          <div>
+            
+          </div>
+
+        </div>
+
+        {/* Таймлайн */}
+        <div>
+          
+        </div>
+
+        {/* Слайдер */}
+        <div>
+          
+        </div>
+
+      </body>
+    </div>
+  );
+}
+
+export default Main;
