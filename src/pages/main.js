f2c924b3feaf6a604af1675ed0bae4a0377ba56b
FelixTisov <fedya002.ks@gmail.com>
2022-04-23 02:53:06 +0300

    оформление титульной части

--- src/pages/main.js
+++ src/pages/main.js
@@ -14,46 +14,45 @@ const images = [
 
 function Main() {
   return (
-    <div className="Main">
-        {/* Главная часть */}
-        <div>
-          {/* header */}
-          <div>
-          <header>
-            <nav>
-              <a href="#история" class = "ss">История</a>
-              <a href="#галерея" class = "ss">Галерея</a>
-              <a href="#карта" class = "ss">Карта</a>
-            </nav>
-          </header>
-          </div>
-          {/* Основная часть */}
-          <div class = "flex-container">
-            <div class ="container1">
-              <div class = "torg">
-                <p class = "torg-text">ТОРГ</p>
-              </div>
-              <div class = "text">
-                <div class = "line-box"><p class="line"></p></div>
-                <div class = "new"><p class="line-text">Торговые ряды — торгово-складской комплекс конца XVIII—начала XIX веков, расположенный в самом центре Костромы. Это образцовый в памятник градостроительного искусства времени екатерининской городской реформы.</p></div>
-              </div>
-              <div class = "button_kart">
-                <button onclick="document.location='pages/map.js'" class="button kart">КАРТА</button>
-              </div>
-            </div>
-            <div class="container2">
-                <div class = "del">
-                  <p class ="ove-text">ОВЫЕ<br/>РЯДЫ</p>
-                </div>
-            </div>
-          </div>
+    <div >
 
+        {/* Титульная часть */}
+        <div className='container'>
+          <div className='flex-block'>
+            <header>
+              <nav>
+                <a href="#история" class = "nav-item">История</a>
+                <a href="#галерея" class = "nav-item">Галерея</a>
+                <a href="#карта" class = "nav-item">Карта</a>
+              </nav>
+            </header>
+
+            <div style={{display: 'flex', zIndex: 1}}>
+              <div class ="ryad" >
+                <div class = "torg">
+                  <p class = "torg-text">ТОРГ</p>
+                </div>
+                <div class = "text">
+                  <div class = "line-box"><p class="line"></p></div>
+                  <div class = "new"><p class="line-text">Торговые ряды — торгово-складской комплекс конца XVIII—начала XIX веков, расположенный в самом центре Костромы. Это образцовый в памятник градостроительного искусства времени екатерининской городской реформы.</p></div>
+                </div>
+                <div class = "button_kart">
+                  <button onclick="document.location='pages/map.js'" class="button kart">КАРТА</button>
+                </div>
+              </div>
+              <div class="ryad" style={{ position: 'relative'}}>
+                  <div class = "del">
+                    <p class ="ove-text">ОВЫЕ<br/>РЯДЫ</p>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* Таймлайн */}
         <div className="felx-block">
 
-          <div className="ryad">
+          <div className="ryad" style={{width: '60%', justifyContent: 'center'}}>
             <div className="model"></div>
           </div>
 
