3845f87c80bb8a2a141d80b7e5a9ef79f2a44947
FelixTisov <fedya002.ks@gmail.com>
2022-04-24 13:49:08 +0300

    мобильная главная

---
+++ src/pages/main_mobile.js
@@ -0,0 +1,23 @@
+import '../styles/main_mobile.css'
+
+function MainMobile() {
+    return(
+        <div className='mainMobile'>
+
+            <div className='cont' id='cont1' >
+                <div>
+
+                </div>
+            </div>
+
+            <div className='cont' id='cont2'>
+                <div>
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MainMobile
