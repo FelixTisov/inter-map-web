9783c180ab5181b8fed8ae4d5ee1e2318c77b13c
FelixTisov <fedya002.ks@gmail.com>
2022-04-24 13:57:01 +0300

    добавлены комментарии

--- src/pages/main_mobile.js
+++ src/pages/main_mobile.js
@@ -4,12 +4,14 @@ function MainMobile() {
     return(
         <div className='mainMobile'>
 
+            {/* Титульная часть */}
             <div className='cont' id='cont1' >
                 <div>
 
                 </div>
             </div>
 
+            {/* Таймлайн */}
             <div className='cont' id='cont2'>
                 <div>
                     
